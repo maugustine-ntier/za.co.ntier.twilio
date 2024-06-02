f09765614fcf1621247dcad47a824af0f2c757a4
martin <martin@martin-VivoBook-ASUSLaptop-X512FB-X512FB>
2024-06-02 10:49:12 +0200

    Add send message process

--- src/za/co/ntier/twilio/process/SendTwilioMessage.java
+++ src/za/co/ntier/twilio/process/SendTwilioMessage.java
@@ -27,22 +27,22 @@ public class SendTwilioMessage extends SvrProcess {
 		X_TW_Message x_TW_Message = new X_TW_Message(getCtx(), record_ID, get_TrxName());
 		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 		if (x_TW_Message.getTwilio_Message_Type().equals(X_TW_Message.TWILIO_MESSAGE_TYPE_SMS)) {			
-	        Message message = Message.creator(
-	                new com.twilio.type.PhoneNumber(x_TW_Message.getPhone()),
-	                new com.twilio.type.PhoneNumber("+14012082693"),
-	                x_TW_Message.getMessage())
-	            .create();
+			Message message = Message.creator(
+					new com.twilio.type.PhoneNumber(x_TW_Message.getPhone()),
+					new com.twilio.type.PhoneNumber("+14012082693"),
+					x_TW_Message.getMessage())
+					.create();
 
-	        System.out.println(message.getSid());
+			System.out.println(message.getSid());
 		} else {  // Assume Whatsapp
-			    Message message = Message.creator(
-			      new com.twilio.type.PhoneNumber("whatsapp:" + x_TW_Message.getPhone()),
-			      new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-			      x_TW_Message.getMessage())
+			Message message = Message.creator(
+					new com.twilio.type.PhoneNumber("whatsapp:" + x_TW_Message.getPhone()),
+					new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
+					x_TW_Message.getMessage())
 
-			    .create();
+					.create();
 
-			    System.out.println(message.getSid());
+			System.out.println(message.getSid());
 		}
 		return "Message Sent";
 	}
